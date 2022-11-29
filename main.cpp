de1bf3427c2c522d9b068b1354cd79bcba90da5a
Jiří Heralt <info@jiriheralt.cz>
2022-11-29 18:41:07 +0100

    Input parameters, parameters validation, output messages

--- main.cpp
+++ main.cpp
@@ -5,22 +5,43 @@
 
 using namespace std;
 
-int main() {
-    string inputFile = "data.txt";
-    int dependenciesFor = 10;
+int main(int argc, char* argv[]) {
 
-    DataImporter dataImporter;
+    // Kontrola vstupnich parametru
+    if (argc < 3) {
+        cerr << "Chybny vstup. Pouziti: " << argv[0] << " VSTUPNI_SOUBOT.txt CISLO_ZMENENEHO_SOUBORU" << endl;
 
-    if (dataImporter.readFile(inputFile) == false) {
         return 1;
     }
 
+    string inputFile = argv[1];
+    int dependenciesFor;
+
+    try {
+        dependenciesFor = stoi(argv[2]);
+    }
+    catch (exception e) {
+        cerr << "Chybny vstup. Druhý parametr musí být integer." << endl;
+        return 1;
+    }
+
+    // Importování dat ze souboru
+    DataImporter dataImporter;
+
+    if (!dataImporter.readFile(inputFile)) {
+        return 1;
+    }
+
+    // Nalezení závislostí
     DependencyFinder finder(dataImporter.getResultGraph());
     finder.findDependenciesFor(dependenciesFor);
 
+    // Vypsání výsledku
+    cout << endl << "Závislé soubory: " << endl;
     for (auto e : finder.getResult()) {
         cout << e << " ";
     }
+    cout << endl;
 
     return 0;
 }
